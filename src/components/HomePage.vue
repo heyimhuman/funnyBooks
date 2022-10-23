f6c67007ea4f1e90d5117f4386a76d9abdaebc98
Zhang <674213286@qq.com>
2022-10-23 14:43:01 +0800

    Modify:CreateProject

---
+++ src/components/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="greetings">
+    <h1>Funny Books List</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+.greetings h1 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1 {
+    text-align: left;
+  }
+}
+</style>
